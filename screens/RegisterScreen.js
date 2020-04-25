a2b00a2ac029f090257e85d9502874304d6bc168
kharioki <antolisah@gmail.com>
2020-04-25 22:36:08 +0300

    added screens

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class RegisterScreen extends Component {
+  state = {};
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Register Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
